718d0c275c65564a12187ff60655e15f49dc53bf
Paul Didi <Paul.e.didi@hotmail.com>
2023-10-06 15:06:53 +0100

    Create a controller and service class and a method to save manufacturers to DB

---
+++ src/main/java/com/learn2code/vehicle/search/vehiclesearch/service/ManufacturerService.java
@@ -0,0 +1,7 @@
+package com.learn2code.vehicle.search.vehiclesearch.service;
+
+import com.learn2code.vehicle.search.vehiclesearch.entity.Manufacturer;
+
+public interface ManufacturerService {
+    Manufacturer saveManufactuer(Manufacturer manufacturer);
+}
